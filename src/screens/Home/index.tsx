bf2e9b32893c797a9e97bf5e8ddb4332fa9585b5
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-09-20 22:25:07 -0300

    Adicionando form com botão e inputText

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,15 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 import {
   styles
 } from './styles';
 
 export function Home() {
+
+  function handleAddParticipant() {
+    console.log('Adicionou participante')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -15,11 +20,19 @@ export function Home() {
         Sexta, 4 de Novembro de 2022
       </Text>
 
-    <TextInput 
-      style={styles.input}
-      placeholder='Nome do participante'
-      placeholderTextColor={'#6B6B6B'}
-    />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
+
+
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,11 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: '#1F1E25',
     borderRadius: 5,
     color: '#FFF',
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31Cf67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 24,
+    color: '#FFF',
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
   }
 })
